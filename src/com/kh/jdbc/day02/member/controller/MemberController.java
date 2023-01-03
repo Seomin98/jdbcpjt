2dd4182223610f8b664998a94c9e1271e337df2b
Seomin98 <tjalsdn1998@gmail.com>
2023-01-03 12:25:37 +0900

    ADD : Day02 회원 관리 프로그램 시작
    1. 메인메뉴 출력 기능 완료
    2. 회원 전체정보 조회 기능 완료
    3. 아이디로 조회 기능 완료
    4. 이름으로 조회 기능 완료(List)
    5. 삭제 기능 완료
    6. 수정 기능 완료

---
+++ src/com/kh/jdbc/day02/member/controller/MemberController.java
@@ -0,0 +1,75 @@
+package com.kh.jdbc.day02.member.controller;
+import java.util.List;
+import com.kh.jdbc.day02.member.model.dao.MemberDAO;
+import com.kh.jdbc.day02.member.model.vo.Member;
+public class MemberController {
+	/**
+	 * 
+	 * @return
+	 */
+	public List<Member> printAll() {	// SELECT * FROM MEMBER_TBL
+		MemberDAO mDao = new MemberDAO();
+		List<Member> mList = mDao.selectAll();
+		return mList;
+	}
+	
+	public Member printOneById(String memberId) { 	// SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ?
+		// ResultSet이 1개면 Member
+		// RseultSet이 1개 이상이면 List<member>
+		MemberDAO mDao = new MemberDAO();
+		Member member = mDao.selectOneById(memberId);
+		return member;
+	}
+	
+	/**
+	 * 이름으로 조회
+	 * @param memberName
+	 * @return
+	 */
+	public List<Member> printMembersByName(String memberName) { // SELECT * FROM MEMBER_TBL WHERE MEMBER_NAME = ?
+		MemberDAO mDao = new MemberDAO();
+		List<Member> mList = mDao.selectMembersByName(memberName);;
+		return mList;
+	}
+	/**
+	 * 회원 로그인
+	 * @param member
+	 * @return
+	 */
+	public int checkInfo(Member member) {	// SELECT * FROM MEMBER_TBL WHERE MEMBER_ID = ? AND MEMBER_PWD = ?
+		MemberDAO mDao = new MemberDAO();
+		int result = mDao.checkLogin(member);
+		return result;
+	}
+
+	/**
+	 * 회원 가입
+	 * @param member
+	 */
+	public int registerMember(Member member) {	// INSERT INTO MEMBER_TBL VALUES(?,?,?,?,?,?,?,?,?,?,DEFAULT)
+		MemberDAO mDao = new MemberDAO();
+		int result = mDao.insertMember(member);
+		return result;
+	}
+	
+	/**
+	 * 회원 탈퇴
+	 * @param memberId
+	 */
+	public int removeMember(String memberId) {	//DELETE FROM MEMBER_TBL WHERE MEMBER_ID = ?
+		MemberDAO mDao = new MemberDAO();
+		int result = mDao.deleteMember(memberId);
+		return result;
+	}
+	
+	/**
+	 * 회원 정보 수정
+	 * @param member
+	 * @return
+	 */
+	public int modifyMemberInfo(Member member) {
+		MemberDAO mDao = new MemberDAO();
+		int result = mDao.updateMember(member);
+		return result;
+	}
+}
---
+++ src/com/kh/jdbc/day02/member/model/vo/Member.java
@@ -0,0 +1,130 @@
+package com.kh.jdbc.day02.member.model.vo;
+
+import java.sql.Timestamp;
+
+public class Member {
+	private String memberId;
+	private String memberPwd;
+	private String memberName;
+	private String MemberGender;
+	private int MemberAge;
+	private String MemberEmail;
+	private String MemberPhone;
+	private String MemberAddress;
+	private String MemberHobby;
+	private Timestamp memberDate;
+	
+	public Member() {
+		
+	}
+
+	
+
+	public Member(String memberId, String memberPwd, String memberName, String memberGender, int memberAge,
+			String memberEmail, String memberPhone, String memberAddress, String memberHobby, Timestamp memberDate) {
+		super();
+		this.memberId = memberId;
+		this.memberPwd = memberPwd;
+		this.memberName = memberName;
+		MemberGender = memberGender;
+		MemberAge = memberAge;
+		MemberEmail = memberEmail;
+		MemberPhone = memberPhone;
+		MemberAddress = memberAddress;
+		MemberHobby = memberHobby;
+		this.memberDate = memberDate;
+	}
+
+
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getMemberPwd() {
+		return memberPwd;
+	}
+
+	public void setMemberPwd(String memberPwd) {
+		this.memberPwd = memberPwd;
+	}
+
+	public String getMemberName() {
+		return memberName;
+	}
+
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+
+	public String getMemberGender() {
+		return MemberGender;
+	}
+
+	public void setMemberGender(String memberGender) {
+		this.MemberGender = memberGender;
+	}
+
+	public int getMemberAge() {
+		return MemberAge;
+	}
+
+	public void setMemberAge(int memberAge) {
+		this.MemberAge = memberAge;
+	}
+
+	public String getMemberEmail() {
+		return MemberEmail;
+	}
+
+	public void setMemberEmail(String memberEmail) {
+		this.MemberEmail = memberEmail;
+	}
+
+	public String getMemberPhone() {
+		return MemberPhone;
+	}
+
+	public void setMemberPhone(String memberPhone) {
+		this.MemberPhone = memberPhone;
+	}
+
+	public String getMemberAddress() {
+		return MemberAddress;
+	}
+
+	public void setMemberAddress(String memberAddress) {
+		this.MemberAddress = memberAddress;
+	}
+
+	public String getMemberHobby() {
+		return MemberHobby;
+	}
+
+	public void setMemberHobby(String memberHobby) {
+		this.MemberHobby = memberHobby;
+	}
+
+	public Timestamp getMemberDate() {
+		return memberDate;
+	}
+
+	public void setMemberDate(Timestamp memberDate) {
+		this.memberDate = memberDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Member [memberId=" + memberId + ", memberPwd=" + memberPwd + ", memberName=" + memberName
+				+ ", MemberGender=" + MemberGender + ", MemberAge=" + MemberAge + ", MemberEmail=" + MemberEmail
+				+ ", MemberPhone=" + MemberPhone + ", MemberAddress=" + MemberAddress + ", MemberHobby=" + MemberHobby
+				+ ", memberDate=" + memberDate + "]";
+	}
+	
+
+	
+}
